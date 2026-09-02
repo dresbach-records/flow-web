a289cd7762af0f343175bf5c67cff5d9da989107
DresbachGroup <dmgproductionsoficial@gmail.com>
2026-09-02 19:09:39 -0300

    style(flow): load dedicated Flow web design system

--- src/main.tsx
+++ src/main.tsx
@@ -3,6 +3,7 @@ import { createRoot } from 'react-dom/client';
 import FlowWeb from './app/FlowWeb';
 import AdminApp from './admin/AdminApp';
 import './styles.css';
+import './app/flow.css';
 
 const root = createRoot(document.getElementById('root')!);
 root.render(location.pathname.startsWith('/admin') ? <AdminApp /> : <FlowWeb />);
