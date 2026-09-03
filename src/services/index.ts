1344cfbb95a73d3a31649b737096c7eabee5117d
DresbachGroup <dmgproductionsoficial@gmail.com>
2026-09-02 21:05:15 -0300

    refactor: expose domain service modules

--- src/services/index.ts
+++ src/services/index.ts
@@ -1,2 +1,7 @@
 export * from './api/client';
 export * from './moduleRegistry';
+export * from './ads';
+export * from './commerce';
+export * from './identity';
+export * from './moderation';
+export * from './rewards';
