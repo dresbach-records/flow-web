72e0386f527e5a0f63fa0e72712ab288b2c1e3f5
DresbachGroup <dmgproductionsoficial@gmail.com>
2026-09-02 21:03:03 -0300

    refactor: establish reusable components layer

---
+++ src/components/index.ts
@@ -0,0 +1,8 @@
+// Shared, reusable UI components belong in this layer.
+export { default as AdminApp } from '../admin/AdminApp';
+export { default as ModuleCenter } from '../admin/ModuleCenter';
+export { default as SiteEditor } from '../admin/SiteEditor';
+export { default as AuthPage } from '../app/AuthPage';
+export { default as CreatorCenter } from '../app/CreatorCenter';
+export { default as PlatformModules } from '../app/PlatformModules';
+export { default as FlowWeb } from '../app/FlowWeb';
