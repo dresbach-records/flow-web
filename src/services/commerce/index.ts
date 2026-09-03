7682df211f14707b8828d247d7a52cd1927f6782
DresbachGroup <dmgproductionsoficial@gmail.com>
2026-09-02 21:04:43 -0300

    refactor: centralize commerce service boundary

---
+++ src/services/commerce/index.ts
@@ -0,0 +1,7 @@
+export * from '../../app/commerce/CommerceFoundation';
+export * from '../../app/commerce/CommerceOperations';
+export * from '../../app/commerce/FlowShopPolicy';
+export * from '../../app/commerce/MarketplaceRules';
+export * from '../../app/commerce/ProductPolicy';
+
+/** Commerce UI remains in pages/components; policies and API orchestration belong here. */
